9d385567862259f88dd045b18df67d93aa426b6d
yamvsne <yamvsne@gmail.com>
2022-10-16 23:46:43 +0900

    ログ用のクラスを追加
    - 指定された行数になったら新しくファイルを作る
    - 定数の大文字化
    - シバンがなぜかpython2だったので3に変更

--- pwm_fan_control.py
+++ pwm_fan_control.py
@@ -1,48 +1,64 @@
-#!/usr/bin/python
-#-*-coding: utf-8 -*-
+#!/usr/bin/python3
+# -*-coding: utf-8 -*-
 
 from datetime import datetime
 import pigpio
 import time
 
-temperature_file = "/sys/class/thermal/thermal_zone0/temp"
-log_file = "/var/log/pwm_fan_control.log"
+TEMPERATURE_FILE_PATH = "/sys/class/thermal/thermal_zone0/temp"
+LOG_SAVE_DIR = "/var/log/"
+SLEEP_TIME = 30  # [s]
+PIN_1 = 18
+FILE_LINES_MAX = 10000
 
 
-# duryは0~1000000(1M)で指定する
-hz = 100 # [Hz]
-duty = 100 # [%]
-sleep_time = 30 # [s]
-pin_1 = 18
+class FanControlLogger:
+    def __init__(self, save_dir: str, file_lines_max: int):
+        self._save_dir = save_dir
+        self._file_name = self._generate_file_name()
+        self._file_lines_max = file_lines_max
+        self._create_file()
 
+    @property
+    def _file_path(self):
+        return self._save_dir + self._file_name
 
-# class FanControlLogger:
-#    self._save_dir = "var/log/"
-#    self._file_name = self._generate_file_name()
-#
-#    def _file_path():
-#        return self._save_dir + self._file_name
-#
-#    def _create_file():
-#        self._file_name = self._generate_file_name()
-#
-#    def _generate_file_name():
-#        now = datetime.now()
-#        date_str = now.strftime("%Y%m%d%H%M%S")
-#        new_name = date_str + "_pwm_fan_control.log"
-#        return new_name
-#
-#    def write(temp, duty):
-#        now = datetime.now()
-#        log = now.strftime('[%Y/%m/%d %H:%M:%S]') \
-#              + " temp: " + str(temp) \
-#              + " duty set: " + str(duty) \
-#              + "\n"
-#        with open(self._file_path(), "a") as f:
-#            f.writelines(log)
+    def _create_file(self):
+        print(f"Create New File: {self._file_path}")
+        open(self._file_path, "w")
+
+    def _generate_file_name(self):
+        now = datetime.now()
+        date_str = now.strftime("%Y%m%d%H%M%S")
+        new_name = "pwm_fan_control.log-" + date_str
+        return new_name
+
+    def _count_file_lines(self):
+        count = sum(1 for _ in open(self._file_path))
+        return count
+
+    def _generate_log_string(self, temp: str, duty: str):
+        now = datetime.now()
+        log = now.strftime('[%Y/%m/%d %H:%M:%S]') \
+            + " temp: " + str(temp) \
+            + " duty set: " + str(duty) \
+            + "\n"
+        return log
+
+    def _write_to_file(self, log: str):
+        with open(self._file_path, "a") as f:
+            f.write(log)
+
+    def write(self, temp: str, duty: str):
+        log = self._generate_log_string(temp, duty)
+        self._write_to_file(log)
+        if self._count_file_lines() >= self._file_lines_max:
+            self._file_name = self._generate_file_name()
+            self._create_file()
+
 
 def get_temp():
-    with open(temperature_file, "r") as f:
+    with open(TEMPERATURE_FILE_PATH, "r") as f:
         data = f.readline()
         temp = float(data)/1000.0
     return temp
@@ -50,11 +66,13 @@ def get_temp():
 
 def main():
     pig = pigpio.pi()
-    pig.set_mode(pin_1, pigpio.OUTPUT)
-    
-    while(True):
+    pig.set_mode(PIN_1, pigpio.OUTPUT)
+    fc_logger = FanControlLogger(LOG_SAVE_DIR, FILE_LINES_MAX)
+
+    while (True):
         temp = get_temp()
         duty = 100
+        hz = 100  # なんで100に設定してるのか忘れてしまった…
 
         if temp >= 90:
             duty = 100
@@ -65,19 +83,12 @@ def main():
         elif 50 > temp:
             duty = 0
 
-        pig.hardware_PWM(pin_1, hz, duty * 10000)
+        pig.hardware_PWM(PIN_1, hz, duty * 10000)
 
-        now = datetime.now()
-        log = now.strftime('[%Y/%m/%d %H:%M:%S]') \
-              + " temp: " + str(temp) \
-              + " duty set: " + str(duty) \
-              + "\n"
-        with open(log_file, "a") as f:
-            f.writelines(log)
+        fc_logger.write(temp, duty)
 
-        time.sleep(sleep_time)
+        time.sleep(SLEEP_TIME)
 
 
 if __name__ == "__main__":
     main()
-
